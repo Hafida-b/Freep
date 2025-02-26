30672bf3ecb059eb4275df8ea58a0df436f75eac
Julien <julien_rubio@yahoo.fr>
2025-02-26 12:29:53 +0100

    update views and serializers

--- backend/freepapp/views.py
+++ backend/freepapp/views.py
@@ -5,6 +5,19 @@ from django.shortcuts import get_object_or_404
 from .models import User, Clothing, Picture, Session
 from .serializers import UserSerializer, SessionSerializer, ClothingSerializer, PictureSerializer
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(["POST"])
 def create_article(request):
