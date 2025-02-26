bcecded69b58f78a8eaeefe05dd7896da5f4e782
Hafida <ha.boukhari31@gmail.com>
2025-02-26 09:46:50 +0100

    Backend clothing list

--- backend/freep/settings.py
+++ backend/freep/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'freepapp.apps.FreepappConfig'
+    'freepapp.apps.FreepappConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
