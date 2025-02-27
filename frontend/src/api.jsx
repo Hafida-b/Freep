5b9263b3b45bd56eb2ddf2d2bb277960e0e87615
Julien <julien_rubio@yahoo.fr>
2025-02-27 11:20:46 +0100

    add handle login errors feature

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -2,17 +2,24 @@ const API_URL = "http://127.0.0.1:8000";
 
 // Fonction pour obtenir les tokens lors de la connexion
 export const fetchToken = async (email, password) => {
-  const response = await fetch(`${API_URL}/api/token/`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
-  });
+  try {
+    const response = await fetch(`${API_URL}/api/token/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
 
-  if (!response.ok) {
-    throw new Error("Erreur lors de la connexion");
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.detail || "Erreur lors de la connexion");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Erreur réseau :", error);
+    throw new Error("Impossible de se connecter au serveur.");
   }
-
-  return response.json();
 };
 
 // Fonction pour rafraîchir le access token
@@ -38,7 +45,9 @@ export const getUserInfo = async (accessToken) => {
   });
 
   if (!response.ok) {
-    throw new Error("Erreur lors de la récupération des informations utilisateur");
+    throw new Error(
+      "Erreur lors de la récupération des informations utilisateur"
+    );
   }
 
   return response.json();
